51ce91cae6066c066a43b4c53a7da8e7d8eea9ca
Tornike Shelia <tornikeshelia1996@gmail.com>
2020-11-11 22:09:07 +0400

    initial commit

---
+++ src/main/java/com/tornikeshelia/gsgyoutubeapi/GsgYoutubeApiApplication.java
@@ -0,0 +1,13 @@
+package com.tornikeshelia.gsgyoutubeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsgYoutubeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsgYoutubeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tornikeshelia/gsgyoutubeapi/GsgYoutubeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tornikeshelia.gsgyoutubeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsgYoutubeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
